0d5bd0e9aa575c75078476b079e3615f4710da77
Olivier Drieux <olivier.drieux@linkweb.fr>
2024-08-26 19:12:05 +0200

    Fly.io

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,7 +1,10 @@
 import Redis from "ioredis";
 
-const REDIS_URL = process.env.REDIS_URL ?? "redis://localhost:6379";
-const connection = new Redis(REDIS_URL, {
+if (!process.env.REDIS_URL) {
+	throw new Error("REDIS_URL is not set");
+}
+
+const connection = new Redis(process.env.REDIS_URL, {
 	maxRetriesPerRequest: null,
 	retryStrategy: (times: number) =>
 		Math.max(Math.min(Math.exp(times), 20000), 1000),
